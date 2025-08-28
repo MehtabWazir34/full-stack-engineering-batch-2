cb4faa2e6af38849a2c4e6469590c740d9e5d3bb
Mehtab Wazir <149592081+MehtabWazir34@users.noreply.github.com>
2025-08-28 23:32:08 +0500

    Update navbar.jsx
    Matched the styles of navBar with footer, same styling, color and effect for both.

--- REACT/Class 6/blog-app/src/components/navbar.jsx
+++ REACT/Class 6/blog-app/src/components/navbar.jsx
@@ -1,14 +1,15 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 function Navbar () {
 
         return (
-            <nav className="w-4xl py-4  mx-auto flex items-center justify-between">
-                <h2 className="text-4xl  font-semibold text-neutral-700">Blogify</h2>
+            <nav className="w-4xl py-4  mx-auto flex items-center justify-between bg-zinc-800 shadow-md sticky top-0 z-50">
+                <h2 className="text-2xl font-bold text-zinc-100 tracking-wide">
+            Blog<span className="text-zinc-400">ify</span> </h2>
                 <ul className="flex items-center gap-10 text-lg ">
-                    <Link className="hover:text-neutral-400 text-neutral-600 transition-all duration-300" to={"/"} >Home</Link>
-                    <Link className="hover:text-neutral-400 text-neutral-600 transition-all duration-300" to={"/create"} >Create</Link>
-                    <Link className="hover:text-neutral-400 text-neutral-600 transition-all duration-300" to={"/profile"} >Profile</Link>
+                    <NavLink className={({isActive}) => (isActive ? 'border-b-2 text-amber-300 font-semibold border-amber-300 pb-1' : " hover:text-zinc-200")} to={"/"} >Home</NavLink>
+                    <NavLink className={({isActive}) => (isActive ? 'border-b-2 text-amber-300 font-semibold border-amber-300 pb-1' : " hover:text-zinc-200")} to={"/create"} >Create</NavLink>
+                    <NavLink className={({isActive}) => (isActive ? 'border-b-2 text-amber-300 font-semibold border-amber-300 pb-1' : " hover:text-zinc-200")} to={"/profile"} >Profile</NavLink>
                 </ul>
             </nav>
         )
