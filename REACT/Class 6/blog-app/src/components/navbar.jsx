a6637c6f569cb8d13268db8f2195aa32609b5066
Mehtab Wazir <149592081+MehtabWazir34@users.noreply.github.com>
2025-08-28 23:00:40 +0500

    Update navbar.jsx
    Styled the logo text Blogify.

--- REACT/Class 6/blog-app/src/components/navbar.jsx
+++ REACT/Class 6/blog-app/src/components/navbar.jsx
@@ -4,7 +4,8 @@ function Navbar () {
 
         return (
             <nav className="w-4xl py-4  mx-auto flex items-center justify-between">
-                <h2 className="text-4xl  font-semibold text-neutral-700">Blogify</h2>
+                <h2 className="text-2xl font-bold text-zinc-100 tracking-wide">
+                Blog<span className="text-zinc-400">ify</span>    </h2>
                 <ul className="flex items-center gap-10 text-lg ">
                     <Link className="hover:text-neutral-400 text-neutral-600 transition-all duration-300" to={"/"} >Home</Link>
                     <Link className="hover:text-neutral-400 text-neutral-600 transition-all duration-300" to={"/create"} >Create</Link>
